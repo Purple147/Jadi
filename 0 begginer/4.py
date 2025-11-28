9206ea7fc2bc98cd1d84e87166eae23ff004d866
Purple147 <gorcraft1380@gmail.com>
2025-11-28 12:43:54 +0330

    refactor: black .

--- 0 begginer/4.py
+++ 0 begginer/4.py
@@ -12,7 +12,9 @@ print("ninth = Although practicality beats purity.")
 print("tenth = Errors should never pass silently.")
 print("eleventh = Unless explicitly silenced.")
 print("twelvth = In the face of ambiguity, refuse the temptation to guess.")
-print("thirteenth = There should be one-- and preferably only one --obvious way to do it.")
+print(
+    "thirteenth = There should be one-- and preferably only one --obvious way to do it."
+)
 print("fourteenth = Although that way may not be obvious at first unless you're Dutch.")
 print("fifteenth = Now is better than never.")
 print("sixteenth = Although never is often better than *right* now.")
@@ -23,9 +25,15 @@ print("twentieth = 4 space for indentation")
 print("twenty_one = using formatter and linter")
 print("twenty_second = resulting by dicts(json), print just for debugs")
 print("twenty_third = use loggin instead of print")
-print("twenty_fourth = more information by type hints(showing classes) and docstring(#notes)")
-print("twenty_fifth = lock every library in requirements.txt to last version until now(example:FasAPI==0.115.4)")
+print(
+    "twenty_fourth = more information by type hints(showing classes) and docstring(#notes)"
+)
+print(
+    "twenty_fifth = lock every library in requirements.txt to last version until now(example:FasAPI==0.115.4)"
+)
 print("twenty_sixth = single source of truth(use env/config)")
 print("twenty_seventh = high quality and small number of comments")
-print("twenty_eighth = code review and continuous integration by merging, no pushing main")
+print(
+    "twenty_eighth = code review and continuous integration by merging, no pushing main"
+)
 print("twenty_ninth = ")

7f6230709e53279c4a491ad6844ee288084d9a96
Purple147 <gorcraft1380@gmail.com>
2025-12-02 11:16:04 +0330

    refactor: eular project 2

---
+++ src/jadi understanding programming/0 begginer/12.py
@@ -0,0 +1,15 @@
+# project eular 2
+def even_numbers(number):
+    return number % 2 == 0
+
+
+number_1 = 0
+number_2 = 1
+summing = 0
+while number_1 < 4 * 10**6:
+    if even_numbers(number_1):
+        summing += number_1
+    number_3 = number_1 + number_2
+    number_1 = number_2
+    number_2 = number_3
+    print(number_1, summing)

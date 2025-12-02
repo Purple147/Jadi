e1d6906f075397b6641c1bd2c624372f3c9d1d34
Purple147 <gorcraft1380@gmail.com>
2025-12-02 20:53:25 +0330

    refactor: libraries and solving jadi's problem, continue

--- src/jadi understanding programming/0 begginer/13.py
+++ src/jadi understanding programming/0 begginer/13.py
@@ -3,12 +3,12 @@ function = ()
 list = []
 set = {}
 dict = {}
-#set and dict havn't priority, dict haven't same number/word, set is faster than list, set haves {|, -, &}
+# set and dict havn't priority, dict haven't same number/word, set is faster than list, set haves {|, -, &}
 random = "Hmideh"
 girls_accademy = ["Amir", "Zhra", "Amir", 77, random]
 girls_accademy.append(99)
 girls_accademy[0]
-girls_accademy[1: 3]
+girls_accademy[1:3]
 girls_accademy[2:]
 girls_accademy[:2]
 girls_accademy[:]
---
+++ src/jadi understanding programming/0 begginer/14.py
@@ -0,0 +1,40 @@
+# libraries, solving jadi's problem, continue
+import random
+random.seed()
+peoples = [100] * 50
+count_numbers = 0
+
+
+for changing in range(1000000):
+    random_number_1 = random.randrange(50)
+    random_number_2 = random.randrange(50)
+    count_numbers += 1
+    if peoples[random_number_1] < 1:
+        continue
+    peoples[random_number_1] -= 1
+    peoples[random_number_2] += 1
+
+
+print("for", count_numbers, "times", peoples)
+
+
+# way 2
+import random
+random.seed()
+count_numbers = 0
+peoples = []
+for room in range(50):
+    peoples.append(100)
+
+
+for changing in range(1000000):
+    random_number_1 = random.randrange(50)
+    random_number_2 = random.randrange(50)
+    count_numbers += 1
+    if peoples[random_number_1] < 1:
+        continue
+    peoples[random_number_1] -= 1
+    peoples[random_number_2] += 1
+
+
+print("for", count_numbers, "times", peoples)

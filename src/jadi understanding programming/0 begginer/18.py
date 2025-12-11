667b2062346cdbfa6307631c37937e86dbe20749
Purple147 <gorcraft1380@gmail.com>
2025-12-11 19:59:06 +0330

    refactot Lybraries and API

---
+++ src/jadi understanding programming/0 begginer/18.py
@@ -0,0 +1,28 @@
+# Readin Standard Libraries of langueg and other Libraris, terminal(CMD), ipython, from, sum
+# Must to doing something, Reading Libraries, Testing in terminal and ipython and editor, complete project
+sum([4, 5])
+sum({3, 12})
+import random
+import webbrowser
+
+import fastapi
+import matplotlib.pyplot as plt
+import pydantic
+import uvicorn
+from fastapi import FastAPI
+
+random.seed()
+random.randrange(234, 234234)
+plt.bar()
+plt.show()
+webbrowser.open()
+fastapi()
+pydantic()
+uvicorn()
+random.random()
+random.randint(4, 111)
+fastapi.FastAPI
+FastAPI.get()
+for ranodm in range(33):
+    print(random.randrange(3, 2342))
+    print(random.randint(3, 2342))
---
+++ src/jadi understanding programming/0 begginer/19.py
@@ -0,0 +1,9 @@
+# Aplication Programming Interface(API), API in a connection between servers
+list = ["Computer", "Labtob", "Mobile", "net"]
+for buying in list:
+    print(list)
+
+
+dictionary = {"Computer", "Labtob", "Mobile", "net"}
+for buying_2 in dictionary:
+    print(buying_2)

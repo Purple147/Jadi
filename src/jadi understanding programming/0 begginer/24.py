6426df4297b4f47d41a7d866754138ca99f61f29
Purple147 <gorcraft1380@gmail.com>
2025-12-12 21:28:15 +0330

    refactor: API

--- src/jadi understanding programming/0 begginer/24.py
+++ src/jadi understanding programming/0 begginer/24.py
@@ -1,4 +1,4 @@
-# API, Deep curl, OAuth 2 = Security API in a Private API in a Public API
+# API, Deep curl, OAuth 2 = Security API in a Private API in a Public API, database
 import requests
 
 API_Key_Storage_ID = "59ecc487e4b03ffa030b24cf"
@@ -15,6 +15,11 @@ Auth = {
     "key": API_Key_Auth_Key,
 }
 Login = requests.get(url_1, auth=Auth)
-Object_Storage = requests.get(url_2)
+Text = {
+    "text": "Fuck You Girl",
+    "average_score": 1,
+    "average_age": 18
+}
+Object_Storage = requests.get(url_2, Text)
 
 print(Auth, Object_Storage)
---
+++ src/jadi understanding programming/0 begginer/26.py
@@ -0,0 +1,42 @@
+# Making a program, ''', """, #TODO, Systems URL, BreakPoin(F9)
+import requests
+API_Key = "4D59644B442B52423271633873314C6B6F2F34413D3D"
+url = "https://api.kavenegar.com/v1/%s/sms/send.json" % API_Key
+api = requests.get(url)
+Sender = "9812949515"
+Receptor = "981231231", "98123123123", "981231231"
+payload = {
+    "message": "hey there, i'm sorry for your time's losting, your time giving is 3 days",
+    "receptor": Receptor,
+    "sender": Sender
+}
+
+requests.post(url, data=payload)
+
+# Way Two
+FileName = "C:/Users/Persatech/Desktop/Phones.txt"
+
+Text = """Hi there, how are you, your book is sending and if you not give a book in a 3 days later
+content with me by godcraft1380@gmail.com, hopely good well
+"""
+
+def Read_Phones(FileName):
+    with open(FileName) as filename:
+        content = filename.readlines()
+    content = [x.strip() for x in content]
+    return content
+
+
+def Send_SMS(Number, Text):
+    API_Key = "7A4F794F53557935526D346C414F673456626E4866413D3D"
+    url = "https://api.kavenegar.com/v1/%s/sms/send.json" % API_Key
+    payload = {"receptor": Number, "massage": Text}
+    api = requests.post(url, data=payload)
+    return api.ok
+
+Phones = Read_Phones(FileName)
+
+
+for Phone in Phones:
+    if not Send_SMS(Phone, Text):
+        print(Phones)

c70b9007afe11b9e35e3bde07ed91f51fc798798
Purple147 <gorcraft1380@gmail.com>
2025-12-22 09:07:40 +0330

    learning: OOP

--- src/Jadi/2 Advance Python/S 1/4.py
+++ src/Jadi/2 Advance Python/S 1/4.py
@@ -1,1 +1,17 @@
-#
+# OOP, Class, Class Variable, Methods, Object Variable, Object Variable = istance,
+class computer:
+    def __init__(self, name, ram, graphic, cpu):
+        self.name = name
+        self.ram = ram
+        self.graphic = graphic
+        self.cpu = cpu
+
+    def inf(self):
+        print(
+            "for %s ram=%i graphic=%i  cpu=%s"
+            % (self.name, self.ram, self.graphic, self.cpu)
+        )
+
+
+Dell = computer("dell", 8, 1, "i5")
+Dell.inf()
